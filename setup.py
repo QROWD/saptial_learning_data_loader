790154fe3d63c820d317b4498cc145b8f8899e12
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2019-03-01 17:37:31 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='spatial_learning_data_loader',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/QROWD/spatial_learning_data_loader',
+    license='Apache License v2.0',
+    author='Patrick Westphal',
+    author_email='patrick.westphal@informatik.uni-leipzig.de',
+    description='',
+    install_requires=[
+        'rdflib==4.2.2',
+        'psycopg2==2.7.7',
+    ],
+    scripts=[
+        'bin/loaddata',
+        'bin/initdb',
+    ],
+)
